06fe336317d6b8e6f4302c8f25dec984b131a235
Alessandro-Moroni <alemoro98@gmail.com>
2024-05-31 17:15:55 +0200

    [milestone]3-4

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+
+  export default {
+    data(){
+      return {
+
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(result =>{
+          console.log(result.data);
+        })
+        .catch(error =>{
+          console.log(error.data);
+
+        })
+      }
+
+    },
+
+    mounted(){
+      this.getApi();
+
+    },
+  }
+</script>
+
+
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
