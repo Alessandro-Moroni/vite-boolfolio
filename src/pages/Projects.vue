2de5946c1d1ff636ca171d6e46e83517d0a4328d
Alessandro-Moroni <alemoro98@gmail.com>
2024-06-04 18:31:29 +0200

    [add]dinamic-route+type in project.show

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,31 +1,61 @@
 <script>
 import {store} from '../data/store';
 import axios from 'axios';
+import Loader from '../components/partials/Loader.vue';
 
    export default {
     name: 'Projects',
 
+    components:{
+      Loader,
+    },
+
     data(){
       return{
         projects: [],
+        technologies: [],
+        types: [],
+        loading: true,
       }
     },
 
     methods:{
-      getApi(){
-        axios.get(store.apiUrl)
+      getApi(apiUrl, type = ''){
+        this.loading = true;
+        axios.get(store.apiUrl + type)
         .then(result =>{
-          this.projects = result.data;
+          
+          this.loading = false;
+
+          switch(type){
+
+            case 'types':
+              this.types = result.data;
+              break;
+
+            case 'technologies':
+              this.technologies = result.data;
+              break;
+
+            default:
+              this.projects = result.data;
+              break;
+           
+          }
         })
         .catch(error =>{
-          this.projects = error.data;
+          this.loading = false;
+          console.log(error);
+         
           
         })
       }
     },
 
     mounted(){
-      this.getApi();
+      this.getApi(store.apiUrl, 'projects');
+      this.getApi(store.apiUrl, 'technologies');
+      this.getApi(store.apiUrl, 'types');
     }
 
      
@@ -38,24 +68,51 @@ import axios from 'axios';
   <div class="container">
 
     <h1>Projects</h1>
+   
+    <div v-if="!loading">
 
-    <div class="row row-cols-3">
 
-      <div class="col mb-3" v-for="project in projects" :key="project.id">
-  
-        <div class="card " style="width: 18rem;">
-          <div class="card-body">
       
-            <h1 class="card-title">{{ project.title }}</h1>
-            <p class="card-text">{{ project.created_at }}</p>
+      <div class="my-3">
+  
+        <span class="badge rounded-pill text-bg-primary me-2" v-for="technology in technologies" :key="technology.id">{{ technology.title }}</span>
+  
+      </div>
+  
+      <div class="my-3">
+  
+        <span class="badge text-bg-success me-2" v-for="item in types" :key="item.id">{{ item.title }}</span>
+  
+      </div>
+  
+      
+  
+      <div class="my-3">
+  
+        <div class="row row-cols-3" >
+    
+          <div class="col mb-3" v-for="project in projects" :key="project.id">
+      
+            <div class="card " style="width: 18rem;">
+              <div class="card-body">
+          
+                <h1 class="card-title">{{ project.title }}</h1>
+                <p class="card-text">{{ project.created_at }}</p>
+                
+              </div>
+            </div>
+    
             
           </div>
+    
         </div>
-
+      </div>
+      
     </div>
     
-
-    </div>
+    <Loader v-else />
+      
+      
 
   </div>
 
